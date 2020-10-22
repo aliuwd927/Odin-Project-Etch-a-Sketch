0d5f8d0125db1c9e8a795692b04240478670c70d
Andy Liu <aliu927@gmail.com>
2020-10-22 01:25:09 -0700

    update

--- js/index.js
+++ js/index.js
@@ -7,14 +7,41 @@ container.appendChild(gridContainer);
 
 //Create a Grid Item Div UNDER the gridContainer
 const gridContainerParent = document.querySelector('.gridContainer');
-//Use for loop to create the div elements
-for(let i = 0; i < 100; i++){
-    //console.log(i); // Prints 1- 16
-    const gridItems = document.createElement('div');
-    gridItems.classList.add('gridItems');
-    //gridItems.textContent = "";
-    gridContainerParent.appendChild(gridItems);
+
+
+const gridLayout = () =>{
+    let gridContainerLayout = document.querySelector('.gridContainer');
+    gridContainerLayout.style.gridTemplateColumns = `repeat(${size}, auto)`;
+    gridContainerLayout.style.gridTemplateRows = `repeat(${size}, auto)`;
 };
+
+
+//Use for loop to create the div elements
+const createGrid = (size) =>{
+    for(let i = 0; i < size; i++){
+        //console.log(i); // Prints 1- 16
+        const gridItems = document.createElement('div');
+        gridItems.classList.add('gridItems');
+        //gridItems.textContent = "";
+        gridContainerParent.appendChild(gridItems);
+    };
+    gridLayout();
+};
+
+
+
+
+const loadPrompt = () =>{
+    //promt('text', deafault Input set by coder);
+    let gridSize = prompt('enter a number no more than 100 for screen size', 32);
+    if(gridSize > 100){
+        gridSize = 100;
+    }
+
+    createGrid(gridSize);
+}
+
+window.onload = loadPrompt();
 
 
 
