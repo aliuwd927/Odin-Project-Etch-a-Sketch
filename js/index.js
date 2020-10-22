92415867e3c28f85d3614c099f59aa29ab9b7233
Andy Liu <aliu927@gmail.com>
2020-10-22 00:42:29 -0700

    update

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const container = document.querySelector('.container');
+
+//Create Grid Div
+const gridContainer = document.createElement('div');
+gridContainer.classList.add('gridContainer');
+container.appendChild(gridContainer);
+
+//Create a Grid Item Div UNDER the gridContainer
+const gridContainerParent = document.querySelector('.gridContainer');
+
+//Use for loop to create the div elements
+
+for(let i = 0; i < 16; i++){
+    //console.log(i); // Prints 1- 16
+    const gridItems = document.createElement('div');
+    gridItems.classList.add('gridItems');
+    gridItems.textContent = i;
+    gridContainerParent.appendChild(gridItems);
+}
+
+
+
+
