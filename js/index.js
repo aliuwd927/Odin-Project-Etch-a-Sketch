0e6f9bd26cf63ab3a31c9d8e3baa35689bbf955e
Andy Liu <aliu927@gmail.com>
2020-10-22 00:53:59 -0700

    update

--- js/index.js
+++ js/index.js
@@ -8,11 +8,11 @@ container.appendChild(gridContainer);
 //Create a Grid Item Div UNDER the gridContainer
 const gridContainerParent = document.querySelector('.gridContainer');
 //Use for loop to create the div elements
-for(let i = 0; i < 32; i++){
+for(let i = 0; i < 100; i++){
     //console.log(i); // Prints 1- 16
     const gridItems = document.createElement('div');
     gridItems.classList.add('gridItems');
-    gridItems.textContent = i;
+    //gridItems.textContent = "";
     gridContainerParent.appendChild(gridItems);
 };
 
