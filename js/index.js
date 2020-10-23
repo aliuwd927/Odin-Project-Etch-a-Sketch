69ea937a42532c4d0a3c1e985df1d5a3042ba7c7
Andy Liu <aliu927@gmail.com>
2020-10-22 22:00:07 -0700

    update js w/ ES6

--- js/index.js
+++ js/index.js
@@ -8,41 +8,29 @@ container.appendChild(gridContainer);
 //Create a Grid Item Div UNDER the gridContainer
 const gridContainerParent = document.querySelector('.gridContainer');
 
-
-const gridLayout = () =>{
-    let gridContainerLayout = document.querySelector('.gridContainer');
-    gridContainerLayout.style.gridTemplateColumns = `repeat(${size}, auto)`;
-    gridContainerLayout.style.gridTemplateRows = `repeat(${size}, auto)`;
-};
-
-
 //Use for loop to create the div elements
-const createGrid = (size) =>{
-    for(let i = 0; i < size; i++){
+const createGrid = (size) => {
+    for(let i = 0; i < size ** 2; i++){
         //console.log(i); // Prints 1- 16
         const gridItems = document.createElement('div');
         gridItems.classList.add('gridItems');
         //gridItems.textContent = "";
         gridContainerParent.appendChild(gridItems);
     };
-    gridLayout();
+    gridLayout(size);
 };
 
+const gridLayout = (size) => {
+    let gridContainerLayout = document.querySelector('.gridContainer');
+    gridContainerLayout.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainerLayout.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+};
 
+let gridSize = prompt('enter a number no more than 100 for screen size', 22);
 
+const getGridSize = (gridSize) > 22 ? gridSize = 22 : '';
 
-const loadPrompt = () =>{
-    //promt('text', deafault Input set by coder);
-    let gridSize = prompt('enter a number no more than 100 for screen size', 32);
-    if(gridSize > 100){
-        gridSize = 100;
-    }
-
-    createGrid(gridSize);
-}
-
-window.onload = loadPrompt();
-
+createGrid(gridSize);
 
 
 
